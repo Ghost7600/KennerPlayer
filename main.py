8304363cafb3995a58519ab01367d25cc18fe71f
kenner.trabalhos@gmail.com <74597805+Ghost7600@users.noreply.github.com>
2023-03-16 00:04:53 -0300

    Implemented volume control and stop button

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 from tkinter import *
 from pygame import mixer
 
+mixer.init() # init mixer
 
-root = Tk()
+
+root = Tk() #creates windows obj
 root.title("Kenner Player")
 root.iconbitmap(r'logo.ico')
 root.geometry('640x480')
+mixer.music.load("Matt_Oakley-Mercury_Ascendant.mp3")
 
 text = Label(root, text = 'The lightest player!')
 text.pack()
 
-photo = PhotoImage(file='play32.png')
+
 #labelphoto = Label(root,image = photo)
 #labelphoto.pack()
-def play_btn():
-    print("button working")
+def playsong():
+    mixer.music.play()
+    print("playing song")
 
-btn = Button(root, image=photo, command = play_btn)
-btn.pack()
+def pausesong():
+    mixer.music.pause()
+
+def setvol(val):
+    vol= int (val)/100
+    mixer.music.set_volume(vol)
+
+playphoto = PhotoImage(file='play32.png')
+playbtn = Button(root, image=playphoto, command = playsong)
+playbtn.pack()
+
+stopbtn=Button(root,text='PAUSE',command = pausesong)
+stopbtn.pack()
+
+scale = Scale(root,from_=0,to=100,orient=HORIZONTAL,command= setvol)
+scale.set(10)
+scale.pack()
 
 root.mainloop()
